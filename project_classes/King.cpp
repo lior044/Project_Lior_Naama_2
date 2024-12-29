6d34df7762330f37ddd2acec5b27023adb41d90f
lior044 <liorscheib@gmail.com>
2024-12-29 17:06:57 +0200

    Finished Invalid moves for king

--- project_classes/King.cpp
+++ project_classes/King.cpp
@@ -9,6 +9,7 @@ bool check_straight_line_threats(char Board[8][8], int king_x, int king_y, int t
 bool check_diagonal_threats(char Board[8][8], int king_x, int king_y, int turn);
 bool check_knight_threats(char Board[8][8], int king_x, int king_y, int turn);
 bool check_pawn_threats(char Board[8][8], int king_x, int king_y, int turn);
+bool check_king_threats(char Board[8][8], int king_x, int king_y, int turn);
 
 int King::check_if_in_check(char Board[8][8], int turn) {
     int king_x = -1, king_y = -1;
@@ -36,7 +37,8 @@ int King::check_if_in_check(char Board[8][8], int turn) {
     if (check_straight_line_threats(Board, king_x, king_y, turn) ||
         check_diagonal_threats(Board, king_x, king_y, turn) ||
         check_knight_threats(Board, king_x, king_y, turn) ||
-        check_pawn_threats(Board, king_x, king_y, turn)) {
+        check_pawn_threats(Board, king_x, king_y, turn) ||
+        check_king_threats(Board, king_x, king_y, turn)) {
         return 1; // King is in check
     }
 
@@ -189,4 +191,44 @@ bool check_pawn_threats(char Board[8][8], int king_x, int king_y, int turn) {
     }
 
     return false;
+}
+bool check_king_threats(char Board[8][8], int king_x, int king_y, int turn) {
+    char enemy_king;
+    if (turn == WHITE) {
+        enemy_king = 'k'; // Black king
+    }
+    else {
+        enemy_king = 'K'; // White king
+    }
+
+    // Define all eight possible directions
+    int directions[8][2] = {
+        {1, 0}, {-1, 0}, {0, 1}, {0, -1}, // Horizontal and vertical
+        {1, 1}, {1, -1}, {-1, 1}, {-1, -1} // Diagonal
+    };
+
+    // Check each direction
+    for (int i = 0; i < 8; i++) {
+        int x = directions[i][0];
+        int y = directions[i][1];
+        int new_x = king_x + x;
+        int new_y = king_y + y;
+
+        // Check bounds and look for the enemy king in the first square in the direction
+        if ((new_x >= 0 && new_x < 8 && new_y >= 0 && new_y < 8) && Board[new_y][new_x] == enemy_king) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int King::Invalid_move(int x_current, int y_current, int x_next, int y_next) {
+    int Invalid = 1;
+
+    if ((x_current == x_next || x_current + 1 == x_next || x_current - 1 == x_next) && (y_current == y_next || y_current + 1 == y_next || y_current - 1 == y_next)) { //make sure that king only moved one step!
+        Invalid = 0;
+    }
+    return Invalid;
+
 }
